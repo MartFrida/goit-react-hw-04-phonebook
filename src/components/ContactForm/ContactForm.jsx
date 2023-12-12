f103c9d36649d57846eed84ee0122252139f4907
Mary <32392607+MartFrida@users.noreply.github.com>
2023-12-12 19:19:04 +0100

    refactoring by hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,36 +1,79 @@
 import { StyledButton, StyledInput } from "components/Phonebook/Phonebook.styled"
-import React from "react";
+import React, { useState } from "react";
 import { StyledContactForm } from "./ContactForm.styled";
 
-export class ContactForm extends React.Component {
-  state = {
-    name: '',
-    number: '',
-  }
-  handleChangeInput = e => {
+export const ContactForm = (onAddContact) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChangeInput = e => {
     const { target } = e;
-    const { name, value } = target;
-    this.setState({ [name]: value })
+    const { value } = target;
+    setName(value);
+    // setNumber(value);
   }
 
-  handleSubmitAddUser = (e) => {
+  const handleSubmitAddUser = (e) => {
     e.preventDefault();
-    this.props.onAddContact(this.state)
-    this.setState({
-      name: '',
-      number: '',
-    })
+    onAddContact(name, number);
+    setName('');
+    setNumber('')
   }
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <StyledContactForm onSubmit={this.handleSubmitAddUser} >
-        <StyledInput name="name" value={name} onChange={this.handleChangeInput} placeholder="Enter name" />
-        <StyledInput name='number' value={number} onChange={this.handleChangeInput} placeholder="Enter phone number" />
-        <StyledButton>Add contact</StyledButton>
-      </StyledContactForm>
-    )
-  }
-
+  return (
+    <StyledContactForm onSubmit={handleSubmitAddUser} >
+      <StyledInput name="name" value={name} onChange={handleChangeInput} placeholder="Enter name" />
+      <StyledInput name='number' value={number} onChange={handleChangeInput} placeholder="Enter phone number" />
+      <StyledButton>Add contact</StyledButton>
+    </StyledContactForm>
+  )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export class ContactForm extends React.Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   }
+//   handleChangeInput = e => {
+//     const { target } = e;
+//     const { name, value } = target;
+//     this.setState({ [name]: value })
+//   }
+
+//   handleSubmitAddUser = (e) => {
+//     e.preventDefault();
+//     this.props.onAddContact(this.state)
+//     this.setState({
+//       name: '',
+//       number: '',
+//     })
+//   }
+//   render() {
+//     const { name, number } = this.state;
+
+//     return (
+//       <StyledContactForm onSubmit={this.handleSubmitAddUser} >
+//         <StyledInput name="name" value={name} onChange={this.handleChangeInput} placeholder="Enter name" />
+//         <StyledInput name='number' value={number} onChange={this.handleChangeInput} placeholder="Enter phone number" />
+//         <StyledButton>Add contact</StyledButton>
+//       </StyledContactForm>
+//     )
+//   }
+
+// }
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,40 +1,21 @@
 import { nanoid } from 'nanoid';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyledWrapper } from './Phonebook.styled';
 import { FilterUsers } from 'components/FilterUsers/FilterUsers';
 import { ContactList } from 'components/ContactList/ContactList';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 
-export class Phonebook extends React.Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  }
+export const Phonebook = () => {
+  const [contacts, setContacts] = useState([
+    // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount = () => {
-    const handleContacts = JSON.parse(window.localStorage.getItem('CONTACTS_DATA'))
-    if (handleContacts?.length) {
-      this.setState({ contacts: handleContacts })
-    }
-  }
-
-  componentDidUpdate = (prevProps, prevState) => {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      window.localStorage.setItem('CONTACTS_DATA', JSON.stringify(this.state.contacts))
-    }
-  }
-
-  componentWillUnmount = () => {
-    window.localStorage.setItem('CONTACTS_DATA', JSON.stringify(this.state.contacts))
-  }
-
-  handleAddUser = ({ name, number }) => {
-    const isExist = this.state.contacts.some((item) => item.name === name)
+  const handleAddUser = ({ name, number }) => {
+    const isExist = contacts.some((item) => item.name === name)
     // console.log();
     if (isExist) {
       alert('ALERT')
@@ -46,31 +27,124 @@ export class Phonebook extends React.Component {
     ))
   }
 
-  handleSetFilter = (e) => {
-    this.setState({ filter: e.target.value })
+  const handleSetFilter = (e) => {
+    setFilter(e.target.value)
   }
 
-  getFilteredContacts = () => {
-    return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()))
-  }
-  handleDeleteUser = (id) => {
-    this.setState(prevState => ({ contacts: prevState.contacts.filter(contact => contact.id !== id) }))
+  const getFilteredContacts = () => {
+    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
   }
 
-  render() {
-    const { filter } = this.state;
-    // console.log(this.getFilteredContacts());
-
-    return (
-      <StyledWrapper >
-        <h2>Phonebook</h2>
-        <ContactForm onAddContact={this.handleAddUser} />
-
-        <h2>Contacts</h2>
-        <FilterUsers filter={filter} handleChangeInput={this.handleSetFilter} />
-        <ContactList filteredContacts={this.getFilteredContacts()} onDeleteUser={this.handleDeleteUser} />
-
-      </StyledWrapper>
-    )
+  const handleDeleteUser = (id) => {
+    setContacts(contacts.filter(contact => contact.id !== id))
   }
+
+  useEffect(() => {
+    const handleContacts = JSON.parse(window.localStorage.getItem('CONTACTS_DATA'))
+    if (handleContacts?.length) {
+      setContacts(handleContacts)
+    }
+  }, [])
+
+  useEffect((prevState) => {
+    if (prevState.length !== contacts.length) {
+      window.localStorage.setItem('CONTACTS_DATA', JSON.stringify(contacts))
+    }
+  }, [contacts])
+
+  useEffect(() => {
+    return window.localStorage.setItem('CONTACTS_DATA', JSON.stringify(contacts))
+  }, [contacts])
+
+
+  return (
+    <StyledWrapper >
+
+      <h2>Phonebook</h2>
+      <ContactForm onAddContact={handleAddUser} />
+      <h2>Contacts</h2>
+      <FilterUsers filter={filter} handleChangeInput={handleSetFilter} />
+      <ContactList filteredContacts={getFilteredContacts()} onDeleteUser={handleDeleteUser} />
+
+    </StyledWrapper>
+  )
 }
+
+
+
+
+
+
+
+
+
+
+// export class Phonebook extends React.Component {
+//   state = {
+//     contacts: [
+//       // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   }
+
+//   componentDidMount = () => {
+//     const handleContacts = JSON.parse(window.localStorage.getItem('CONTACTS_DATA'))
+//     if (handleContacts?.length) {
+//       this.setState({ contacts: handleContacts })
+//     }
+//   }
+
+//   componentDidUpdate = (prevProps, prevState) => {
+//     if (prevState.contacts.length !== this.state.contacts.length) {
+//       window.localStorage.setItem('CONTACTS_DATA', JSON.stringify(this.state.contacts))
+//     }
+//   }
+
+//   componentWillUnmount = () => {
+//     window.localStorage.setItem('CONTACTS_DATA', JSON.stringify(this.state.contacts))
+//   }
+
+//   handleAddUser = ({ name, number }) => {
+//     const isExist = this.state.contacts.some((item) => item.name === name)
+//     // console.log();
+//     if (isExist) {
+//       alert('ALERT')
+//       return
+//     }
+//     this.setState(prevState => ({
+//       contacts: [...prevState.contacts, { id: nanoid(), name, number }]
+//     }
+//     ))
+//   }
+
+//   handleSetFilter = (e) => {
+//     this.setState({ filter: e.target.value })
+//   }
+
+//   getFilteredContacts = () => {
+//     return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+//   }
+//   handleDeleteUser = (id) => {
+//     this.setState(prevState => ({ contacts: prevState.contacts.filter(contact => contact.id !== id) }))
+//   }
+
+//   render() {
+//     const { filter } = this.state;
+//     // console.log(this.getFilteredContacts());
+
+//     return (
+//       <StyledWrapper >
+//         <h2>Phonebook</h2>
+//         <ContactForm onAddContact={this.handleAddUser} />
+
+//         <h2>Contacts</h2>
+//         <FilterUsers filter={filter} handleChangeInput={this.handleSetFilter} />
+//         <ContactList filteredContacts={this.getFilteredContacts()} onDeleteUser={this.handleDeleteUser} />
+
+//       </StyledWrapper>
+//     )
+//   }
+// }
